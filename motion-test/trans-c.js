8bef7380d605e4e73a5a604eaca5457abb2ec172
Richard Stovall <rstovall@me.com>
2013-11-17 20:22:37 -0500

    working out the 3rd animation - needed a more “watery” effect

---
+++ motion-test/trans-c.js
@@ -0,0 +1,97 @@
+var qz = {};
+
+qz.transx = function(elem, arr, num) {
+	var el;
+	var panelSize;
+	var panelLeft;
+	var panelRight;
+	var cssTime = 5000;
+
+	(function(){init()})();
+
+	function init() {
+		if(elem && arr && num) {
+			if(el = document.getElementById(elem)) {
+				setSizing(num);
+				window.addEventListener('load', function(){
+				  	el.setAttribute('style','visibility:visible;');
+				});
+			} else { throw new Error('an element with the id of "' + elem + '" was not found'); }
+		} else { throw new Error('required parameter(s) missing'); }
+	};
+
+	function setSizing(num) {
+		var w = el.clientWidth;
+		var s = w % num;
+
+		if(!s) {
+			panelSize = panelLeft = panelRight = w / num;
+		} else {
+			panelSize = Math.floor(w / num);
+			if(!(s % 2)) {
+				panelLeft = panelRight = (panelSize + s / 2);
+			} else {
+				panelLeft = panelSize + Math.ceil(s / 2);
+				panelRight = panelSize + Math.floor(s / 2);
+			}
+		}
+		createPanels();
+	};
+
+	function createPanels() {
+		var panel;
+		var top;
+
+		for(var i = 0; i < num; i++){
+			panel = document.createElement('div');
+			panel.className = 'panel';
+			top = document.createElement('div');
+			top.className = 'top';
+			top.style.backgroundImage = "url('"+ arr[1] +"')";
+
+			if(i == 0) {
+				panel.setAttribute('style','width:' + panelLeft + 'px');
+				top.style.backgroundPosition = "0px 0px";
+			} else if(i == (num - 1)) {
+				panel.setAttribute('style','width:' + panelRight + 'px');
+				top.style.backgroundPosition = "-" + (panelLeft + (panelSize * (num - 2))) + "px 0px";
+			} else {
+				panel.setAttribute('style','width:' + panelSize + 'px');
+				top.style.backgroundPosition = "-" + ((panelSize * i) + (panelLeft - panelSize)) + "px 0px";
+			}
+
+			panel.appendChild(top);
+			el.appendChild(panel);
+		}
+	};
+
+	function doOut(i, a) {
+		var p = document.getElementsByClassName('panel');
+		var t = factorial() / num;
+
+		setTimeout(function(){
+			setTimeout(function(){
+				a.className = a.className += " move";
+				a.style.backgroundPosition = (i * 250 + 1100) + 'px 0px';
+			}, (num - i) * 100);
+
+			setTimeout(function(){
+				p[i].style.display = 'none';
+			}, (num - i) * t);
+		}, i);
+	};
+
+	function factorial() { 
+		var result = 0;
+		for(var i = 0;i < num;i++) { result += i * 100 }
+		return result;
+	} 
+
+	qz.transx.out = function() {
+		var a = document.getElementsByClassName('top');
+
+		for(var i = a.length - 1;i >= 0;i--) {
+			doOut(i, a[i]);
+		}
+	};
+}
